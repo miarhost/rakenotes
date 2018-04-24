99ba3e7c3f0c565416e45a5b6f3cdc4413465f82
MariaTikhonova <ritz.rkraft@gmail.com>
2018-04-24 21:44:00 +0300

    add some links and assets

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,26 @@ Rails.application.routes.draw do
 
 	root 'static_pages#home'
 
-  get '/home',to: 'static_pages#home'
+  get '/home', to: 'static_pages#home'
 
-  get '/about',to: 'static_pages#about'
+  get '/about', to: 'static_pages#about'
 
-  get '/contacts',to: 'static_pages#contacts'
+  get '/contacts', to: 'static_pages#contacts'
 
-  get '/literature',to: 'static_pages#literature'
+  get '/literature', to: 'static_pages#literature'
 
-  get '/explanations',to: 'static_pages#explanations'
+  get '/explanations', to: 'static_pages#explanations'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/deploy', to: 'static_pages#deploy'
+
+  get '/heroku', to: 'static_pages#heroku'
+
+  get '/suse', to: 'static_pages#suse'
+
+  get 'home(/:deploy)', to: 'static_pages#home'
+  
+  get 'home(/:heroku)', to: 'static_pages#home'
+
+  get 'home(/:suse)', to: 'static_pages#home'
+ 
 end
