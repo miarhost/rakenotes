8fbae6020e080c4e9c94b872fae5931d110694bb
MariaTikhonova <ritz.rkraft@gmail.com>
2018-06-13 14:39:54 +0300

    rspec model tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ 
+ before do
+ 	subject { described_class.new(:title => "Example Post", :body => "Welcome, the first user's post")}
+ end
+
+ describe "ActiveModel validations" do
+ 
+  it "should validate presence of title" do
+  	expect(subject).to validate_presence_of(:title)
+  	expect(response).to be_success
+  end
+
+  it "should validate presense of text" do
+  	expect(subject).to validate_presence_of(:body)
+  end
+  
+ end
+
+ describe "ActiveRecord associations" do
+
+   it "should belong to user" do
+   	expect(subject).to belong_to(:user)
+   end
+
+ end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    subject { described_class.new(:name => "User", :email => "user@example.com", :password => "1234567", :password_confirmation => "1234567") }
+  end
+
+  describe "ActiveModel validations" do
+
+  it "should not be valid without email" do
+  	subject.email = nil
+  	expect(user).not_to be_valid
+    expect(subject).to validate_presence_of(:email)
+    expect(response).to be_success
+  end
+
+  it "should not be valid without password" do
+  	subject.password = nil
+  	expect(user).not_to be_valid
+    expect(subject).to validate_presence_of(:password)
+    expect(response).to be_success
+  end
+
+  it "should validate email's length" do
+   expect(subject.email.size).should be < 255
+  end
+
+  it "should validate password confirmation" do
+    expect(subject).to validate_presence_of(:password_confirmation)
+    expect(password_confirmation).to eql(:password)
+    expect(response).to be_success
+  end
+
+ end
+
+ describe "ActiveRecord associations" do
+
+  it "should have posts" do
+    expect(subject).to have_many(:posts)
+  end    
+  
+ end
+
 end
