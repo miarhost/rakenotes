bd97ce9f70b70b79f027a4e56810640849b4b56e
MariaTikhonova <ritz.rkraft@gmail.com>
2018-06-19 20:28:37 +0300

    subscription controller spec

--- spec/controllers/subscription_controller_spec.rb
+++ spec/controllers/subscription_controller_spec.rb
@@ -2,3 +2,19 @@ require 'rails_helper'
 
 RSpec.describe SubscriptionsController, type: :controller do
 
+
+
+describe "POST #create" do
+	it "creates new subscription" do
+	subscription = Subscription.create :email
+	expect(response).to be_success
+	expect(ActionMailer::SubscriptionMailer.subscribe(:email)).to :deliver_later
+   end
+ end
+ 
+end
+
+
+
+		
+
